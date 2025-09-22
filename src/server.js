073204ac5d3a6176f90471bf88d11caca77049c6
VinhDDT <3dtv.thevinh14052004@gmail.com>
2025-09-22 09:33:29 +0700

    push 22/9

--- src/server.js
+++ src/server.js
@@ -4,5 +4,6 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(); 
+  console.log(`http://localhost:${PORT}`);
 });
